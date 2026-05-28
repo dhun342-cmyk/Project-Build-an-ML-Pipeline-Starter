17b3c06899ea8f59ed8962502b5399abae9864b8
D Hunter <dhun342@wgu.edu>
2026-05-27 17:46:34 -0700

    Fix indentation for random forest pipeline step

--- main.py
+++ main.py
@@ -98,14 +98,14 @@ def go(config: DictConfig):
 
     
 
-            # NOTE: we need to serialize the random forest configuration into JSON
-            rf_config = os.path.abspath("rf_config.json")
-            with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+        # NOTE: we need to serialize the random forest configuration into JSON
+        rf_config = os.path.abspath("rf_config.json")
+        with open(rf_config, "w+") as fp:
+            json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-         ##################
+        # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
+        # step
+        ##################
         if "train_random_forest" in active_steps:
             _ = mlflow.run(
                 f"{config['main']['src']}/train_random_forest",
